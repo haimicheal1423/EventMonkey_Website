0658c1fe63585b36b87c47908eafccaa8ad19f08
Mattlee0610 <mlee56@mail.sfsu.edu>
2022-10-20 16:58:32 -0700

    set up functions for features

---
+++ applications/server/features/accountFeatures.js
@@ -0,0 +1,38 @@
+/*
+This file is used to hold the functions that operate mainly with the events within the
+Event Monkey application. The features inside this file are:
+- addEvent()    --> Letting the user add personal events into the database
+- deleteEvent() --> Letting the user delete person events that they added to the database
+- searchEvent() --> Search the database for events
+- sortEvents()  --> Allow for displaying events based on specific event categories
+- loadEvent()   --> Load the event from the database into frontend
+- favorEvent()  --> Allow the user to add events to a favorites list
+- remFavEvent() --> Allow the user to remove events to a favorites list
+*/
+function addEvent() {
+
+}
+
+function deleteEvent() {
+
+}
+
+function searchEvent() {
+
+}
+
+function sortEvents() {
+
+}
+
+function loadEvent() {
+
+}
+
+function favorEvent() {
+
+}
+
+function remFavEvent() {
+
+}
---
+++ applications/server/features/miscFeatures.js
@@ -0,0 +1,27 @@
+/*
+This file is to hold the functions for more miscellaneous features. These features include:
+- displayCalendar() --> Displays the events inside the user's calendar
+- addFriend()       --> Allows user to add a friend to their friend's list
+- deleteFriend()    --> Allows user to remove friend from friend's list
+- deleteNotif()     --> Allows user to remove notifications
+- displayNotif()    --> Displays the notifications the user received
+*/
+function displayCalendar() {
+
+}
+
+function addFriend() {
+
+}
+
+function deleteFriend() {
+
+}
+
+function deleteNotif() {
+
+}
+
+function displayNotif() [
+
+]
