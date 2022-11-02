1d25596810ed3bd38ce5b27634b9470917d1a906
Michael Maksoudian <mmaksoudian@mail.sfsu.edu>
2022-11-02 14:46:52 -0700

    removed dead code and unnecessary comment

--- applications/server/server.js
+++ applications/server/server.js
@@ -1,9 +1,8 @@
-const { application } = require("express");
 const express = require("express");
-const path = require("path");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: ".env-local" });
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -15,12 +14,5 @@ const eventRouter = require("./routes/event");
 
 app.use("/users", userRouter);
 app.use("/events", eventRouter);
-
-// //set static folder
-// const homePage = path.join(__dirname, "../client/home.html");
-// // home
-// app.get("/", (req, res)=>{
-//     res.sendFile(homePage);
-// });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
