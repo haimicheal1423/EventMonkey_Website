62a563835ddbdd67fbbceccc27a83b9c273a33e8
Michael Maksoudian <mmaksoudian@mail.sfsu.edu>
2022-11-02 14:45:52 -0700

    reformatted server file

--- applications/server/server.js
+++ applications/server/server.js
@@ -3,16 +3,15 @@ const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
 
-dotenv.config({path:".env-local"});
+dotenv.config({ path: ".env-local" });
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 const userRouter = require("./routes/user");
-const eventRouter = require("./routes/event")
-
+const eventRouter = require("./routes/event");
 
 app.use("/users", userRouter);
 app.use("/events", eventRouter);
@@ -24,5 +23,4 @@ app.use("/events", eventRouter);
 //     res.sendFile(homePage);
 // });
 
-
-app.listen(PORT, () =>  console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
