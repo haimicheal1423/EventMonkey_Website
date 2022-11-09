382f634a0f9214c6ee8e161afa6bd4d176d200f4
Mattlee0610 <mlee56@mail.sfsu.edu>
2022-11-09 14:46:43 -0800

    fixed testing files

--- applications/server/tests/testFile.test.js
+++ applications/server/tests/testFile.test.js
@@ -1,6 +1,6 @@
 // Note that the file name is filename.test.js
 
-const sum = require('../features/testFileExample.js');
+const sum = require('./testFileExample.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
