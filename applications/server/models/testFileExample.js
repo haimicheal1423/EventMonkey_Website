379d964ac0296ac750ae24024cc03cd5dfb1c8f0
Mattlee0610 <mlee56@mail.sfsu.edu>
2022-11-18 17:24:36 -0800

    Solved testing issue; now testing github actions

--- applications/server/models/testFileExample.js
+++ applications/server/models/testFileExample.js
@@ -1,3 +1,5 @@
-export default function add(a, b) {
-    return a+b;
+export class testClass {
+    testFunction(str1, str2) {
+        return str1.concat(' ', str2);
+    }
 }
--- applications/server/tests/testFile.test.js
+++ applications/server/tests/testFile.test.js
@@ -1,25 +1,9 @@
-import add from '../models/testFileExample.js'
+import { testClass } from '../models/testFileExample.js'
 
+const testingClass = new testClass;
 //console.log('../functionsFolder/testingImport.js');
 describe('testFileExample.js', function() {
-    test('add two numbers together', function() {
-        expect(add(1, 2)).toBe(3);
+    test('Testing class exports', function() {
+        expect(testingClass.testFunction('Hello', 'world')).toBe('Hello world');
     });
 })
-
-/*
-To install jest and babel:
-npm install --save-dev jest
-npm install @babel/preset-env --save-dev
-*/
-
-/*
-ERROR: FAILED IMPORT
-
-It seems like the error comes from having another package.json file inside the server folder. It may be
-causing a conflict of some kind that results it returning the error:
-    SyntaxError: Cannot use import statement outside a module
-If the package.json file in the server folder was removed, tests with import run fine.
-I attempted to use modulePathIgnorePatterns and passing in the server package.json file, but it seems to
-have no effect.
-*/
