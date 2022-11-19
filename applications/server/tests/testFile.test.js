ab44b851b12dff266bca1312125f90ebfe30dd9d
Mattlee0610 <mlee56@mail.sfsu.edu>
2022-11-18 16:38:03 -0800

    Error message with tests when there is a conflicting package.json file

--- applications/server/tests/testFile.test.js
+++ applications/server/tests/testFile.test.js
@@ -1,7 +1,25 @@
-// Note that the file name is filename.test.js
+import add from '../models/testFileExample.js'
 
-const sum = require('./testFileExample.js');
+//console.log('../functionsFolder/testingImport.js');
+describe('testFileExample.js', function() {
+    test('add two numbers together', function() {
+        expect(add(1, 2)).toBe(3);
+    });
+})
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+/*
+To install jest and babel:
+npm install --save-dev jest
+npm install @babel/preset-env --save-dev
+*/
+
+/*
+ERROR: FAILED IMPORT
+
+It seems like the error comes from having another package.json file inside the server folder. It may be
+causing a conflict of some kind that results it returning the error:
+    SyntaxError: Cannot use import statement outside a module
+If the package.json file in the server folder was removed, tests with import run fine.
+I attempted to use modulePathIgnorePatterns and passing in the server package.json file, but it seems to
+have no effect.
+*/
