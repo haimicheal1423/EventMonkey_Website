5d003d872e37c797bcffaad7bf9454311441962b
Michael Maksoudian <mmaksoudian@mail.sfsu.edu>
2022-11-23 11:36:21 -0800

    fixed a bug with reading ticket master event dates;
    improved how the client displays event dates;
    
    separated local helper functions into their own named functions in client/Event.js

--- applications/event-monkey/src/pages/Event.js
+++ applications/event-monkey/src/pages/Event.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
-import Axios from 'axios';
 import { useSearchParams } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/Row';
+import Axios from 'axios';
 
 function Event() {
     const columnsPerRow = 4;
@@ -25,80 +24,8 @@ function Event() {
         }
     }, []);
 
-    const eventImage = event => {
-        if (event.images[0]) {
-            return event.images[0].url;
-        } else {
-            return 'holder.js/100px180?text=Image cap';
-        }
-    };
-
-    const priceRange = event => {
-        if (!event.priceRanges || event.priceRanges.length === 0) {
-            return (
-                <ListGroup.Item>
-                    <Card.Text>
-                        Price: Free
-                    </Card.Text>
-                </ListGroup.Item>
-            )
-        }
-
-        const formatPrice = (currency, price) => {
-            if (currency === 'USD') {
-                return `$${price}`;
-            } else {
-                return `${price} ${currency}`;
-            }
-        }
-
-        return (
-            <ListGroup.Item>{
-                event.priceRanges.map(range => {
-                    const min = formatPrice(range.currency, range.min.toFixed(2));
-                    const max = formatPrice(range.currency, range.max.toFixed(2));
-
-                    return (
-                        <Card.Text key={range.currency}>
-                            Price: {
-                                range.min === range.max
-                                ? max
-                                : `${min} - ${max}`
-                            }
-                        </Card.Text>
-                    );
-                })
-            }</ListGroup.Item>
-        );
-    };
-
-    const eventDate = event => {
-        const formatter = Intl.DateTimeFormat('en-us', {
-            weekday: "long",
-            year: "numeric",
-            month: "short",
-            day: "numeric"
-        });
-
-        const startDate = new Date(event.dates.startDateTime);
-
-        let endDate;
-        if (event.dates.endDateTime) {
-            endDate = new Date(event.dates.endDateTime);
-        }
-
-        return (
-            <ListGroup.Item>
-                <Card.Text>
-                    {formatter.format(startDate)}
-                    {endDate && ' until ' + formatter.format(endDate)}
-                </Card.Text>
-            </ListGroup.Item>
-        );
-    };
-
     return (
-        <Container>
+        <>
             <Row xs={1} md={columnsPerRow}>
                 {events?.length > 0 && events.map(event =>
                     <Col>
@@ -131,7 +58,91 @@ function Event() {
                     </Col>
                 )}
             </Row>
-        </Container>
+        </>
+    );
+}
+
+function eventImage(event) {
+    if (event.images[0]) {
+        return event.images[0].url;
+    } else {
+        return 'holder.js/100px180?text=Image cap';
+    }
+}
+
+function priceRange(event) {
+    if (!event.priceRanges || event.priceRanges.length === 0) {
+        return (
+            <ListGroup.Item>
+                <Card.Text>
+                    Price: Free
+                </Card.Text>
+            </ListGroup.Item>
+        )
+    }
+
+    const formatPrice = (currency, price) => {
+        if (currency === 'USD') {
+            return `$${price}`;
+        } else {
+            return `${price} ${currency}`;
+        }
+    }
+
+    return (
+        <ListGroup.Item>
+            {event.priceRanges.map(range => {
+                const min = formatPrice(range.currency, range.min.toFixed(2));
+                const max = formatPrice(range.currency, range.max.toFixed(2));
+
+                return (
+                    <Card.Text key={range.currency}>
+                        Price: {
+                            range.min === range.max
+                                ? max
+                                : `${min} - ${max}`
+                        }
+                    </Card.Text>
+                );
+            })}
+        </ListGroup.Item>
+    );
+}
+
+function eventDate(event) {
+    const formatter = Intl.DateTimeFormat('en-us', {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    });
+
+    let startDate;
+    if (event.dates.startDateTime) {
+        startDate = new Date(event.dates.startDateTime);
+    }
+
+    let endDate;
+    if (event.dates.endDateTime) {
+        endDate = new Date(event.dates.endDateTime);
+    }
+
+    if (!startDate && !endDate) {
+        return (
+            <ListGroup.Item>
+                <Card.Text>{'Date TBA'}</Card.Text>
+            </ListGroup.Item>
+        );
+    }
+
+    return (
+        <ListGroup.Item>
+            {startDate && <Card.Text>{formatter.format(startDate)}</Card.Text>}
+            {endDate && <Card.Text>until</Card.Text>}
+            {endDate && <Card.Text>{formatter.format(endDate)}</Card.Text>}
+        </ListGroup.Item>
     );
 }
 
