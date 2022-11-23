e8d2e4c9bb706537cc8a54a52c23286c651fa850
Michael Maksoudian <mmaksoudian@mail.sfsu.edu>
2022-11-22 21:56:09 -0800

    fixed event search feature in the client

--- applications/event-monkey/src/pages/Event.js
+++ applications/event-monkey/src/pages/Event.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-import { Routes, Route, useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Col from 'react-bootstrap/Col';
@@ -9,40 +9,118 @@ import Row from 'react-bootstrap/Row';
 
 function Event() {
     const columnsPerRow = 4;
-    const { genre } = useParams()
-    const [events, setEvents] = useState([])
-    const getAllEvents = () => {
-        if (genre) {
-            Axios.get(`http://localhost:4000/events/${genre}`).then((response) => {
-                setEvents(response.data);
-            });
-        } else {
-            Axios.get("http://localhost:4000/events").then((response) => {
-                setEvents(response.data);
-            });
-        }
-    }
+    const [searchParams] = useSearchParams();
+    const [events, setEvents] = useState([]);
 
     useEffect(() => {
-        getAllEvents();
+        searchParams.forEach((val, key) => console.log(key, val));
+
+        if (searchParams.has('source') || searchParams.has('limit')
+                || searchParams.has('keyword') || searchParams.has('genres')) {
+            Axios.get(`http://localhost:4000/events/search?${searchParams}`)
+                .then(response => setEvents(response.data));
+        } else {
+            Axios.get("http://localhost:4000/events")
+                .then(response => setEvents(response.data));
+        }
     }, []);
+
+    const eventImage = event => {
+        if (event.images[0]) {
+            return event.images[0].url;
+        } else {
+            return 'holder.js/100px180?text=Image cap';
+        }
+    };
+
+    const priceRange = event => {
+        if (!event.priceRanges || event.priceRanges.length === 0) {
+            return (
+                <ListGroup.Item>
+                    <Card.Text>
+                        Price: Free
+                    </Card.Text>
+                </ListGroup.Item>
+            )
+        }
+
+        const formatPrice = (currency, price) => {
+            if (currency === 'USD') {
+                return `$${price}`;
+            } else {
+                return `${price} ${currency}`;
+            }
+        }
+
+        return (
+            <ListGroup.Item>{
+                event.priceRanges.map(range => {
+                    const min = formatPrice(range.currency, range.min.toFixed(2));
+                    const max = formatPrice(range.currency, range.max.toFixed(2));
+
+                    return (
+                        <Card.Text key={range.currency}>{
+                            range.min === range.max
+                            ? max
+                            : `${min} - ${max}`
+                        }
+                        </Card.Text>
+                    );
+                })
+            }</ListGroup.Item>
+        );
+    };
+
+    const eventDate = event => {
+        const formatter = Intl.DateTimeFormat('en-us', {
+            weekday: "long",
+            year: "numeric",
+            month: "short",
+            day: "numeric"
+        });
+
+        const startDate = new Date(event.dates.startDateTime);
+
+        let endDate;
+        if (event.dates.endDateTime) {
+            endDate = new Date(event.dates.endDateTime);
+        }
+
+        return (
+            <ListGroup.Item>
+                <Card.Text>
+                    {formatter.format(startDate)}
+                    {endDate && ' until ' + formatter.format(endDate)}
+                </Card.Text>
+            </ListGroup.Item>
+        );
+    };
 
     return (
         <Container>
             <Row xs={1} md={columnsPerRow}>
-                {events?.length > 0 && events.map((event, index) =>
+                {events?.length > 0 && events.map(event =>
                     <Col>
                         <Card style={{ width: '18rem', margin:20, padding:20 }}>
-                            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                            <Card.Img variant="top" src={eventImage(event)} />
                             <Card.Body>
-                                <Card.Title>Name: {event.name}</Card.Title>
-                                <Card.Text>
-                                    Description: {event.description}
-                                </Card.Text>
+                                <Card.Title>
+                                    {event.name}
+                                </Card.Title>
+                                <Card.Text>{
+                                    event.description.length > 90
+                                        ? event.description.substring(0, 87) + ' [...]'
+                                        : event.description
+                                }</Card.Text>
                             </Card.Body>
                             <ListGroup className="list-group-flush">
-                                <ListGroup.Item>{event.price}</ListGroup.Item>
-                                <ListGroup.Item>{event.dates}</ListGroup.Item>
+                                {priceRange(event)}
+                                {eventDate(event)}
+                                <ListGroup.Item>
+                                    <Card.Text>
+                                        {event.location}
+                                    </Card.Text>
+                                </ListGroup.Item>
                             </ListGroup>
                             <Card.Body>
                                 <Card.Link href="#">Card Link</Card.Link>
@@ -50,7 +128,6 @@ function Event() {
                             </Card.Body>
                         </Card>
                     </Col>
-
                 )}
             </Row>
         </Container>
