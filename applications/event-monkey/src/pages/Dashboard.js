538daf16dbc6527ad4f9397149e00c1c7b621f55
Michael Maksoudian <mmaksoudian@mail.sfsu.edu>
2022-11-25 16:03:55 -0800

    added a friends list ui to the user dashboard;
    saving the user object response from login to local storage to use in different pages

--- applications/event-monkey/src/pages/Dashboard.js
+++ applications/event-monkey/src/pages/Dashboard.js
@@ -4,37 +4,86 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useNavigate } from 'react-router';
+import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 function Dashboard() {
     const navigate = useNavigate();
-    return (
-        <Container>
-            <Row>
-                <Col></Col>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Welcome Sajan!</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroup.Item>Sajan Gururng</ListGroup.Item>
-                            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                            <ListGroup.Item  onClick={() => {
-                                localStorage.setItem('token',false);
-                                navigate('/');
-                            }}>Logout</ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-                <Col></Col>
-            </Row>
+    const [user, setUser] = useState(undefined);
+    const [friendsList, setFriendsList] = useState([]);
 
-        </Container>
+    useEffect(() => {
+        setUser(JSON.parse(localStorage.getItem('user')));
+    }, []);
+
+    useEffect(() => {
+        if (!user) {
+            return;
+        }
+        Axios.get(`http://localhost:4000/users/${user.id}/friends`)
+            .then(response => void setFriendsList(response.data))
+            .catch(e => alert(e.data));
+    }, [user]);
+
+    if (!user) {
+        return <>Loading...</>;
+    }
+
+    return (
+        <>
+            <Container className='d-inline-flex'>
+                <Row className='m-auto'>
+                    <Col>
+                        <Card className='m-3 p-3' style={{ width: '18rem' }}>
+                            <Card.Img variant='top' src={user.profileImage ? user.profileImage.url : 'holder.js/100px180?text=Image cap'} />
+                            <Card.Body>
+                                <Card.Title>Welcome {user.username}!</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className='list-group-flush'>
+                                <ListGroup.Item>{user.username}</ListGroup.Item>
+                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Button variant='danger' onClick={() => {
+                                        localStorage.setItem('token', 'false');
+                                        localStorage.removeItem('user');
+                                        navigate('/');
+                                    }}>Logout</Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className='mx-3 my-3'>
+                    <Col className='d-flex flex-column align-items-baseline'>
+                        <h5>Friends</h5>
+                        <hr className='my-2 w-100' />
+                        <form className='d-flex'>
+                            <input
+                                type='text'
+                                className='form-control border-warning'
+                                placeholder='Enter friend name'/>
+                            <Button variant='primary' className='ml-2'>Add</Button>
+                            <Button variant='danger' className='ml-2'>Remove</Button>
+                        </form>
+                        <div className='d-flex overflow-auto' style={{width: '52rem', minHeight: '10rem'}}>
+                            {friendsList.map(friend => {
+                                return (
+                                    <Card key={friend.id} className='mr-3 my-3' style={{width: '12rem', minWidth: '12rem'}}>
+                                        <Card.Img variant='top' src={friend.profileImage ? friend.profileImage.url : 'holder.js/100px180?text=Image cap'} />
+                                        <Card.Footer>{friend.username}</Card.Footer>
+                                    </Card>
+                                );
+                            })}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 }
 
